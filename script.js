73edb835cd763db565eb0af5bd1f5f78cd222303
Tanvi <166649868+itstanvi@users.noreply.github.com>
2024-08-13 13:43:32 +0530

    WEATHER APP
    i successfully made weather site using html css js and API

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-//window.alert("hello world");
+ async function weather(){
+    const cityname=document.querySelector('#search input').value;
+    console.log(cityname);
+    const url=`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityname}&appid=757faaceca15cd840edd86f0322ab9d5`;
+    const res= await fetch(url);
+   // console.log(res);
+   var data= await res.json();
+   console.log(data.main.temp);
+   if(data.code='404'){
+    error.style.display="block";
+    main.style.display="none";
+   }
+   else{
+    temp.innerHTML=Math.round(data.main.temp)+"°C";
+    document.getElementById('city').innerText=data.name;
+    document.getElementById('humidity').innerHTML=data.main.humidity+"%";
+    document.getElementById('wind').innerHTML=data.wind.speed+"km/h";
+    main.style.display="block";
+    error.style.display="none";
+   }
 
-function clock(){
-    //xwindow.alert("hello");
-   const d=new Date;
-   //console.log(d)
-   let h=d.getHours();
-   let m=d.getMinutes();
-   let s=d.getSeconds();
-   console.log(h,m,s);
-   let h_rotation=30*h+0.5*m;
-   let m_rotation=6*m;
-   let s_rotation=6*s; 
-   hr.style.transform=`rotate(${h_rotation}deg)`;
-   min.style.transform=`rotate(${m_rotation}deg)`;
-   sec.style.transform=`rotate(${s_rotation}deg)`;
-   dc.innerHTML=`${h}:${m}:${s}`;
-   
-
-   const ame=d.toLocaleTimeString("en-US",{
-    timeZone:"America/Los_Angeles",
-   });
-   console.log(ame)
 
 }
-clock();
-setInterval(clock,1000);
+
